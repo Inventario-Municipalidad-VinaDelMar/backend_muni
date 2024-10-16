c27131f2124edaf2030f9c9128d98fcc934c6f03
H33Criss <agartkg95@gmail.com>
2024-10-16 18:42:45 -0300

    fix(seed_data): delete tildes en nombres de usuarios

--- src/seed/data/seed-data.ts
+++ src/seed/data/seed-data.ts
@@ -55,10 +55,10 @@ interface SeedData {
 
 export const initialData: SeedData = {
     users: [
-        { email: 'cristobal@gmail.com', password: 'Abc12345', nombre: 'Cristóbal', apellidoPaterno: 'Herrera', apellidoMaterno: 'Rojas', rut: '20440649-9', roles: ['administrador'] },
+        { email: 'cristobal@gmail.com', password: 'Abc12345', nombre: 'Cristobal', apellidoPaterno: 'Herrera', apellidoMaterno: 'Rojas', rut: '20440649-9', roles: ['administrador'] },
         { email: 'mangini@gmail.com', password: 'Abc12345', nombre: 'Franco', apellidoPaterno: 'Mangini', apellidoMaterno: 'Tapia', rut: '20175289-2', roles: ['administrador'] },
         { email: 'diego@gmail.com', password: 'Abc12345', nombre: 'Diego', apellidoPaterno: 'Hidalgo', apellidoMaterno: 'Carvajal', rut: '21069070-0', roles: ['administrador'] },
-        { email: 'renato@gmail.com', password: 'Abc12345', nombre: 'Renato', apellidoPaterno: 'Plaza', apellidoMaterno: 'Díaz', rut: '20482871-7' },
+        { email: 'renato@gmail.com', password: 'Abc12345', nombre: 'Renato', apellidoPaterno: 'Plaza', apellidoMaterno: 'Diaz', rut: '20482871-7' },
     ],
     planificaciones: [
         {
